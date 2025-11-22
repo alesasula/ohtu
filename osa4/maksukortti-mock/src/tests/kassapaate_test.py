d1966b47be2536a2f101abb3c2c5117850024662
Aleksi Sulasalmi <aleksi.s.sulasalmi@student.jyu.fi>
2025-11-23 00:26:42 +0200

    lataa metodin testit lisatty

--- osa4/maksukortti-mock/src/tests/kassapaate_test.py
+++ osa4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,18 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lisaa_saldoa_kortille_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, 9)
+
+        maksukortti_mock.lataa.assert_called_with(9)
+
+    def test_lisaa_saldoa_kortille_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, -8)
+
+        maksukortti_mock.lataa.assert_not_called()
+
